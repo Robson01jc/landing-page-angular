2ad56de3180d08c4e614d22dc22a7f7d48ed21c8
Robson Carvalho <robson01jc@gmail.com>
2022-06-16 13:06:24 -0300

    Subject + scan operator

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { scan, Subject } from 'rxjs';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationsService {
+  messages: Subject<Command>;
 
-  constructor() { }
+  constructor() {
+    this.messages = new Subject<Command>();
+  }
+
+  getMessages() {
+    return this.messages.pipe(
+      scan((acc, value) => {
+        if (value.type === 'clear') {
+          return acc.filter((message) => message.id !== value.id);
+        }
+
+        return [...acc, value];
+      }, [] as Command[])
+    );
+  }
+
+  addSuccess(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success',
+    });
+  }
+
+  addError(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error',
+    });
+  }
+
+  clearMessage(id: number) {
+    this.messages.next({
+      id,
+      type: 'clear',
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
